643bcc29d0c2a0a2897d912cfdd974c18d764cf1
xion <calf-love@naver.com>
2023-05-12 16:06:10 +0900

    initial commit

---
+++ shoppingApp/src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './modules/index.ts';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);

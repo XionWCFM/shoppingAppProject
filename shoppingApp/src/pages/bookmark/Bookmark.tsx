35e0cad418f7aea12ee0ab9bbe0cd05255da412b
xion <calf-love@naver.com>
2023-05-15 01:44:26 +0900

    update main and bookmark

--- shoppingApp/src/pages/bookmark/Bookmark.tsx
+++ shoppingApp/src/pages/bookmark/Bookmark.tsx
@@ -1,5 +1,41 @@
+import MainContainer from '../../components/layouts/MainContainer';
+import Loading from '../../components/loading/Loading';
+import CardContainer from '../../components/productCard/CardContainer';
+import {
+  CozApiInterface,
+  useGetProductQuery,
+} from '../../modules/cozShoppingAPI';
+
 const Bookmark = () => {
-  return <div>북마크페이지입니다.</div>;
+  const { data, isError, isLoading } = useGetProductQuery(undefined);
+
+  if (isLoading || isError || !data)
+    return (
+      <MainContainer>
+        <>
+          <Loading />
+        </>
+      </MainContainer>
+    );
+
+  const bookMarkData = data.filter(
+    (apiData: CozApiInterface) => apiData.bookmark,
+  );
+  console.log(bookMarkData.length);
+  return (
+    <MainContainer>
+      <>
+        <h2 className=" text-2xl font-bold">북마크 리스트</h2>
+        {bookMarkData.length === 0 ? (
+          <p className=" py-7">북마크 된 상품이 없습니다.</p>
+        ) : (
+          <CardContainer apiData={bookMarkData}>
+            <h2 className=" text-2xl font-bold">북마크 리스트</h2>
+          </CardContainer>
+        )}
+      </>
+    </MainContainer>
+  );
 };
 
 export default Bookmark;

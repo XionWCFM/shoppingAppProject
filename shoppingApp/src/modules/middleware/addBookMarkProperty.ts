908adbca3dda39d238a7db1a0f1d31926e5a5eb9
xion <calf-love@naver.com>
2023-05-15 10:49:57 +0900

    create localstorage logic

--- shoppingApp/src/modules/middleware/addBookMarkProperty.ts
+++ shoppingApp/src/modules/middleware/addBookMarkProperty.ts
@@ -9,10 +9,20 @@ const addBookmarkProperty: Middleware = () => (next) => (action) => {
   );
 
   if (action.type.endsWith('fulfilled')) {
-    action.payload = action.payload.map((data: CozApiInterface) => ({
-      ...data,
-      bookmark: false,
-    }));
+    action.payload = action.payload.map((data: CozApiInterface) => {
+      const localDataIdx = getBookMarkData.findIndex(
+        (localData: CozApiInterface) => localData.id === data.id,
+      );
+
+      return localDataIdx === -1
+        ? { ...data, bookmark: false }
+        : { ...data, bookmark: true };
+    });
+
+    // ({
+    //   ...data,
+    //   bookmark: false,
+    // })
   }
   return next(action);
 };

9efe00b047ea0b0b407d0e3d1b3538b95f0515a1
xion <calf-love@naver.com>
2023-05-16 14:17:34 +0900

    update toastType isVisible delete

--- shoppingApp/src/utils/createToastMessage.ts
+++ shoppingApp/src/utils/createToastMessage.ts
@@ -3,13 +3,11 @@ const createToastMessage = (bookmark: boolean) => {
     ? {
         id: Number(new Date()),
         content: '북마크에 추가 되었습니다.',
-        isVisible: true,
         bookmark: true,
       }
     : {
         id: Number(new Date()),
         content: '북마크에서 제거 되었습니다.',
-        isVisible: true,
         bookmark: false,
       };
 
